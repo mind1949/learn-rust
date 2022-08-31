9fb0291282ee44f9a9899aa27f20a86e51cc2159
mind1949 <lianjie1949@gmail.com>
2022-08-31 21:04:54 +0800

    complete chapter 2

--- command_line_rust/echor/src/main.rs
+++ command_line_rust/echor/src/main.rs
@@ -23,5 +23,5 @@ fn main() {
     let text = matches.values_of_lossy("text").unwrap();
     let omit_newline = matches.is_present("omit_newline");
 
-    print!("{}{}", text.join(" "), if omit_newline { ""} else { "\n"})
+    print!("{}{}", text.join(" "), if omit_newline { "" } else { "\n" })
 }
--- command_line_rust/echor/tests/cli.rs
+++ command_line_rust/echor/tests/cli.rs
@@ -1,19 +1,47 @@
 use assert_cmd::Command;
 use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd =  Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
-    .failure()
-    .stderr(predicate::str::contains("USAGE"));
+        .failure()
+        .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
 fn runs() {
     let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("hello")
-    .assert()
-    .success();
+    cmd.arg("hello").assert().success();
 }
 
+#[test]
+fn hello1() -> TestResult {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> TestResult {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> TestResult {
+    run(&["Hello there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> TestResult {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
+}
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    let expected = fs::read_to_string(expected_file)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+    cmd.args(args).assert().success().stdout(expected);
+    Ok(())
+}
