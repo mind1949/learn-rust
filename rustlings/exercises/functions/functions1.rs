2383a5bf4bb4a070ec69db10c851f81976d44378
mind1949 <lianjie1949@gmail.com>
2022-09-14 23:40:17 +0800

    rustlings: 64

---
+++ rustlings/exercises/functions/functions1.rs
@@ -0,0 +1,10 @@
+// functions1.rs
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
+
+fn main() {
+    call_me();
+}
+
+fn call_me() {
+    println!("call me");
+}
